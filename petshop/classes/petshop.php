137402294a31685a41bfbe922aa891bae3788569
Cruelkid <akosovich@narod.ru>
2017-07-10 14:29:49 +0300

    some changes in petshop

--- petshop/classes/petshop.php
+++ petshop/classes/petshop.php
@@ -3,9 +3,12 @@ require_once 'classes/pet.php';
 require_once 'models/cat.php';
 require_once 'models/dog.php';
 require_once 'models/hamster.php';
+require_once 'db.json';
 
 class PetShop
 {
+	public $pets =[];
+
 	public function getAvgPrice($arr)
 	{
 		$length = count($arr);
@@ -45,5 +48,50 @@ class PetShop
 		}
 
 		return $res;
+	}
+
+	public function getDataFromFile()
+	{
+		$data = file_get_contents("db.json");
+		$result = json_decode($data, true);
+		$length = count($result);
+		// d($length);
+
+		foreach ($result as $pet) {
+			$length = count($pet);
+			for ($i = 0; $i < $length; $i++) { 
+				if (count($pet[$i]) == 4) {
+					$newCat = new Cat($pet->name, $pet->color, $pet->fluff, $pet->price);
+					array_push($this->pets, $newCat);
+				} elseif (count($pet[$i]) == 3) {
+					$newDog = new Dog($pet->name, $pet->color, $pet->price);
+					array_push($this->pets, $newDog);
+				} elseif (count($pet[$i]) == 2) {
+					$newHamster = new Hamster($pet->color, $pet->price);
+					array_push($this->pets, $newHamster);
+				}
+				// _d(count($pet[$i]));
+			}
+		}
+
+		// for ($i = 0; $i < $length; $i++) {
+		// 	d(key($result));
+			// if (key($result[$i]) == "Cats") {
+			// 	foreach ($result as $cat) {
+			// 		$newCat = new Cat($cat->name, $cat->color, $cat->fluff, $cat->price);
+			// 		array_push($this->pets, $newCat);
+			// 	}
+			// } elseif (key($result[$i]) == "Dogs") {
+			// 	foreach ($result as $dog) {
+			// 		$newDog = new Dog($dog->name, $dog->color, $dog->price);
+			// 		array_push($this->pets, $newDog);
+			// 	}
+			// } elseif (key($result[$i]) == "Hamsters") {
+			// 	foreach ($result as $hamster) {
+			// 		$newHamster = new Hamster($hamster->color, $hamster->price);
+			// 		array_push($this->pets, $newHamster);
+			// 	}
+			// }
+		// }
 	}
 }
--- petshop/index.php
+++ petshop/index.php
@@ -4,25 +4,34 @@ require_once 'classes/pet.php';
 require_once 'models/cat.php';
 require_once 'models/dog.php';
 require_once 'models/hamster.php';
+require_once 'db.json';
 
 function d($t){echo "<xmp>".print_r($t, 1)."</xmp>";}
 function _d($t){d($t);die;}
 
-$cat1 = new Cat("Abbath", "black", 2, 666);
-$cat2 = new Cat("Horgh", "white", 4, 500);
-$dog1 = new Dog("Hoest", "grey", 700);
-$dog2 = new Dog("Gaahl", "white", 600);
-$hmstr1 = new Hamster("orange", 13);
-$hmstr2 = new Hamster("grey", 11);
+$result = file_get_contents("db.json");
+$decoded = json_decode($result);
+d($decoded);
+_d($decoded->Cats[0]);
 
-$pets = [];
-array_push($pets, $cat1);
-array_push($pets, $cat2);
-array_push($pets, $dog1);
-array_push($pets, $dog2);
-array_push($pets, $hmstr1);
-array_push($pets, $hmstr2);
+// $cat1 = new Cat("Abbath", "black", 2, 666);
+// $cat2 = new Cat("Horgh", "white", 4, 500);
+// $dog1 = new Dog("Hoest", "grey", 700);
+// $dog2 = new Dog("Gaahl", "white", 600);
+// $hmstr1 = new Hamster("orange", 13);
+// $hmstr2 = new Hamster("grey", 11);
+
+// $pets = [];
+// array_push($pets, $cat1);
+// array_push($pets, $cat2);
+// array_push($pets, $dog1);
+// array_push($pets, $dog2);
+// array_push($pets, $hmstr1);
+// array_push($pets, $hmstr2);
 $ps = new PetShop();
+
+_d($ps->getDataFromFile());
+
 echo "Average price: " . $ps->getAvgPrice($pets) . "<br/>";
 echo "<br/>";
 $petsByColor = $ps->getByColor("white", $pets);
